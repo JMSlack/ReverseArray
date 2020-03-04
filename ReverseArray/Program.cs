06362c35b3536f7f28e76cb6f767dcd7ae2213f1
Jonathan Slack <jonathan.m.slack@outlook.com>
2020-03-04 08:20:32 -0500

    Add project files.

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] result = ReverseString("lamp");
+            string reversed = new string(result);
+            Console.WriteLine(reversed);
+
+        }
+
+        public static char[] ReverseString (string input)
+        {
+            char[] userString = input.ToCharArray();
+            int length = userString.Length;
+            char holder;
+            for (int i = 0, j = length -1; i < length / 2; i++, j--)
+            {
+                holder = userString[i];
+                userString[i] = userString[j];
+                userString[j] = holder;
+
+            }
+
+            return userString;
+        }
+
+
+
+    }
+}
